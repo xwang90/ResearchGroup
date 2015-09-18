e69e9ece45cbcdffec3d834fe758b466add466e2
Xiaoliang Wang <xwang90@binghamton.edu>
2015-09-17 22:44:58 -0400

    Reorganize some comment

--- src/xlw/android/researchgroup/MainActivity.java
+++ src/xlw/android/researchgroup/MainActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package xlw.android.researchgroup;
 
 
@@ -25,18 +9,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-/**
- * The launchpad activity for this sample project. This activity launches other activities that
- * demonstrate implementations of common animations.
- */
 public class MainActivity extends ListActivity {
 	
 	public static FavoriteDataSource datasource;
 	
-    /**
-     * This class describes an individual sample (the sample title, and the activity class that
-     * demonstrates this sample).
-     */
     private class Sample {
         private CharSequence title;
         private Class<? extends Activity> activityClass;
@@ -52,11 +28,6 @@ public class MainActivity extends ListActivity {
         }
     }
 
-    /**
-     * The collection of all samples in the app. This gets instantiated in {@link
-     * #onCreate(android.os.Bundle)} because the {@link Sample} constructor needs access to {@link
-     * android.content.res.Resources}.
-     */
     private static Sample[] mSamples;
 
     public void onCreate(Bundle savedInstanceState) {
@@ -69,7 +40,6 @@ public class MainActivity extends ListActivity {
         datasource = new FavoriteDataSource(this);
         datasource.open();
 
-        // Instantiate the list of samples.
         mSamples = new Sample[]{
                 new Sample(R.string.Analog, ScreenSlideAnalogActivity.class),
                 new Sample(R.string.ComputerArchitecture, ScreenSlideComputerArchitectureActivity.class),
@@ -87,7 +57,7 @@ public class MainActivity extends ListActivity {
 
     @Override
     protected void onListItemClick(ListView listView, View view, int position, long id) {
-        // Launch the sample associated with this list position.
+        
         startActivity(new Intent(MainActivity.this, mSamples[position].activityClass));
     }
     
